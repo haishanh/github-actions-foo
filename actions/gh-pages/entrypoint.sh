bb8ec80905527cecd33aaa80d3574e8519fc2ff8
Haishan <haishanhan@gmail.com>
2019-01-13 17:08:27 +0800

    test

--- actions/gh-pages/entrypoint.sh
+++ actions/gh-pages/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-set -xe
+set -e
 
 BUILD_DIR="${BUILD_DIR:-build}"
 BRANCH="${BRANCH:-gh-pages}"
 
 cd $BUILD_DIR
 
-# remote="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-remote="https://github.com/${GITHUB_REPOSITORY}.git"
+remote="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+# remote="https://github.com/${GITHUB_REPOSITORY}.git"
 git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@noreply.github.com"
