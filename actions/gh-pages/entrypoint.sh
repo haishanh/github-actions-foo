ca657c227b937156889a02492ffedab164f5e170
Haishan <haishanhan@gmail.com>
2019-02-19 23:59:10 +0800

    update

--- actions/gh-pages/entrypoint.sh
+++ actions/gh-pages/entrypoint.sh
@@ -9,7 +9,6 @@ cd $BUILD_DIR
 
 # NOTE push with GITHUB_TOKEN won't trigger a gh-pages deployment
 remote="https://${TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-# remote="https://github.com/${GITHUB_REPOSITORY}.git"
 git init
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@noreply.github.com"
